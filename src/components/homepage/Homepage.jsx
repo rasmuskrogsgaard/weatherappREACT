27fe7556c0dc975b22f880c155cb29c11f41cbea
Rasmus Krogsgaard <rasmus.krogsgaard@gmail.com>
2023-08-29 14:15:08 +0200

    ola

---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+const HomePage = () => {
+
+const [data, setData] = useState([])
+
+useEffect(())
+    return (
+
+
+<div>
+<h1></h1>
+
+
+</div>
+
+
+)
+}
