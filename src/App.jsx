b95f0e8ec92eef9c50e75876690afae02ab9758a
Morten010 <97233551+Morten010@users.noreply.github.com>
2023-09-01 10:16:34 +0200

    Geolocation permissions

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import './App.css'
 import HomePage from './components/homepage/Homepage.jsx'
 import { useTheme } from './components/providers/ThemeProvider'
-import DarkModeButton from './components/ui/DarkModeButton'
 import { Nav } from './components/Nav/Nav';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Weekpage from './pages/Weekpage';
@@ -22,6 +21,22 @@ function App() {
   }
 
   navigator.geolocation.getCurrentPosition(success, error)
+
+  navigator.permissions.query({ name: "geolocation" })
+  .then((permissionStatus) => {
+    if(permissionStatus.state === 'denied'){
+      setLocation(false)
+    } else if(permissionStatus.state === "granted"){
+      setLocation(true)
+    }
+    permissionStatus.onchange = () => {
+      if(permissionStatus.state === 'denied'){
+        setLocation(false)
+      } else if(permissionStatus.state === "granted"){
+        setLocation(true)
+      }
+    };
+  });
   
 
   const style = theme === "dark" ? {
@@ -32,15 +47,16 @@ function App() {
     color: "#121212"
   }
 
+  console.log(location)
   return (
     <main style={style}>
 
       <BrowserRouter>
         <Nav/>
         <Routes>
-          <Route index element={<HomePage />} />
-          <Route path='/uge' element={<Weekpage />} />
-          <Route path='/about' element={<About />} />
+          <Route index element={ location ? <HomePage /> : <Navigate to="/allow" />} />
+          <Route path='/uge' element={location ? <Weekpage /> : <Navigate to="/allow" />} />
+          <Route path='/about' element={ location ? <About /> : <Navigate to="/allow" />} />
           <Route index element={location ? <HomePage /> : <Navigate to="/allow" />} />
           <Route path='/uge' element={location ? <Weekpage /> : <Navigate to="/allow" />} />
           <Route path='/allow' element={!location ? <Allowpage /> : <Navigate to="/" />} />
